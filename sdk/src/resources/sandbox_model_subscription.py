f83d2a351e7e8cc60e83749a345349be1d80ff39
Rafael Pierre <rafaelpierre@github.com>
2025-01-09 14:15:40 +0100

    model sub

--- sdk/src/resources/sandbox_model_subscription.py
+++ sdk/src/resources/sandbox_model_subscription.py
@@ -1,15 +1,32 @@
 from src.utils import AzureCredentialSingleton
+from azure.ai.ml import MLClient
+from azure.ai.ml.entities import MarketplaceSubscription
 import logging
 
 logging.basicConfig(level = "DEBUG")
 
-def main():
+def create_model_subscription(
+    model_id: str,
+    subscription_name: str,
+    subscription_id: str,
+    resource_group_name: str,
+    workspace_name: str
+):
 
-    credential = AzureCredentialSingleton().get_credential()
-    token = credential.get_token(".default")
-    logging.info(f"Token: {token}")
+    client = MLClient(
+        credential=AzureCredentialSingleton().get_credential(),
+        subscription_id=subscription_id,
+        resource_group_name=resource_group_name,
+        workspace_name=workspace_name
+    )
 
+    marketplace_subscription = MarketplaceSubscription(
+        model_id=model_id,
+        name=subscription_name,
+    )
 
-if __name__ == "__main__":
-
-    main()
+    marketplace_subscription = (
+        client
+        .marketplace_subscriptions
+        .begin_create_or_update(marketplace_subscription)
+    ).result()
