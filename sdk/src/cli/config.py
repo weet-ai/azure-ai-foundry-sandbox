c8e484780a91208b4c9a6e4af017bd33f2d2888d
Rafael Pierre <rafaelpierre@github.com>
2025-01-15 11:46:56 +0100

    cli

--- sdk/src/cli/config.py
+++ sdk/src/cli/config.py
@@ -1,5 +1,6 @@
 import os
-
+from azure.ai.ml import MLClient
+from sdk.src.utils import AzureCredentialSingleton
 
 class ClientConfig(object):
 
@@ -7,3 +8,26 @@ class ClientConfig(object):
 
         self.subscription_id = os.getenv("AZURE_SUBSCRIPTION_ID")
         self.resource_group_name = os.getenv("AZURE_RESOURCE_GROUP_NAME")
+        self.workspace_name = os.getenv("AZURE_AI_FOUNDRY_PROJECT_NAME")
+
+class MLClientFacade(MLClient):
+
+    def __init__(self, config: ClientConfig = None):
+
+        if not config:
+            config = ClientConfig()
+
+        self.subscription_id = config.subscription_id
+        self.resource_group_name =config.resource_group_name
+        self.workspace_name = config.workspace_name
+
+    def get_instance(self):
+
+        client = MLClient(
+            credential=AzureCredentialSingleton().get_credential(),
+            subscription_id=self.subscription_id,
+            resource_group_name=self.resource_group_name,
+            workspace_name=self.workspace_name,
+        )
+
+        return client
--- sdk/src/cli/main.py
+++ sdk/src/cli/main.py
@@ -1,9 +1,40 @@
-"""
-client = MLClient(
-    credential=AzureCredentialSingleton().get_credential(),
-    subscription_id=subscription_id,
-    resource_group_name=resource_group_name,
-    workspace_name=workspace_name,
-)
-"""
+"""CLI Entrypoints for Sandbox functionality."""
 
+from sdk.src.cli.config import ClientConfig, MLClientFacade
+import click
+import logging
+
+logging.basicConfig(level = "DEBUG")
+
+
+@click.group()
+def sandbox():
+    click.echo("Azure AI Foundry Sandbox")
+    pass
+
+# Subscription group
+@sandbox.group()
+def subscription():
+    """Manage subscriptions."""
+    pass
+
+@subscription.command("create")
+@click.argument("model_id")
+def create_subscription(model_id):
+    """Create a subscription for the given MODEL_ID."""
+    client = MLClientFacade()
+
+# Endpoint group
+@sandbox.group()
+def endpoint():
+    """Manage endpoints."""
+    pass
+
+@endpoint.command("create")
+@click.argument("model_id")
+def create_endpoint(model_id):
+    """Create an endpoint for the given MODEL_ID."""
+    click.echo(f"Endpoint created for model_id: {model_id}")
+
+if __name__ == "__main__":
+    sandbox()
